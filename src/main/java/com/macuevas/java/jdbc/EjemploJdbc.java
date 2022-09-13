028d6039c2fe63a0a57fafd1d96986492fec1f4f
miguel angel cuevas <106960689+ermigue2022@users.noreply.github.com>
2022-09-13 20:22:52 +0200

    11. Cerrando conexiones, recursos y manejo de errores

--- src/main/java/com/macuevas/java/jdbc/EjemploJdbc.java
+++ src/main/java/com/macuevas/java/jdbc/EjemploJdbc.java
@@ -7,22 +7,29 @@ public class EjemploJdbc {
         String url = "jdbc:mysql://localhost:3306/java_curso";
         String username = "root";
         String password = "root";
-        try {
 
-            Connection conn = DriverManager.getConnection(url,username,password);
+
+        /* Try con recurso, la finalización de las conexiones son automáticas*/
+        try(Connection conn = DriverManager.getConnection(url,username,password);
             Statement stmt = conn.createStatement();
-            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos")) {
 
             while (resultado.next()){
-                System.out.println(resultado.getString("nombre"));
+                System.out.print(resultado.getInt("id"));
+                System.out.print("  |  ");
+                System.out.print(resultado.getString("nombre"));
+                System.out.print("  |  ");
+                System.out.print(resultado.getInt("precio"));
+                System.out.print("  |  ");
+                System.out.println(resultado.getDate("fecha_registro"));
+
 
             }
-            resultado.close();
-            stmt.close();
-            conn.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+
+
         }
     }
 }
