7f7b1d7324934ae13f5f9c0169f666230c24568e
miguel angel cuevas <106960689+ermigue2022@users.noreply.github.com>
2022-09-13 13:36:12 +0200

    10. Creando nuestro proyecto Java con JDBC

---
+++ src/main/java/com/macuevas/java/jdbc/EjemploJdbc.java
@@ -0,0 +1,28 @@
+package com.macuevas.java.jdbc;
+
+import java.sql.*;
+
+public class EjemploJdbc {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/java_curso";
+        String username = "root";
+        String password = "root";
+        try {
+
+            Connection conn = DriverManager.getConnection(url,username,password);
+            Statement stmt = conn.createStatement();
+            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+
+            while (resultado.next()){
+                System.out.println(resultado.getString("nombre"));
+
+            }
+            resultado.close();
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
