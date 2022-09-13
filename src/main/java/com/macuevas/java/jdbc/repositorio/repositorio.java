0eb64ebc1bcc4ddc5e6f8b99e91b2d361a2f2f9d
miguel angel cuevas <106960689+ermigue2022@users.noreply.github.com>
2022-09-13 23:20:49 +0200

    13. la interface Repositio

---
+++ src/main/java/com/macuevas/java/jdbc/repositorio/repositorio.java
@@ -0,0 +1,15 @@
+package com.macuevas.java.jdbc.repositorio;
+
+import java.util.List;
+
+public interface repositorio<T>{
+
+    List<T> listar();
+
+    T porId(Long id);
+
+    void guardar(T t); /* reutilizamos para insertar o modificar */
+
+    void eliminar(Long id);
+
+}
